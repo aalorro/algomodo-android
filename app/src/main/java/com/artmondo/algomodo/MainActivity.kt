65d7b3dec35a1114b7ed8bb09131f763698063d2
aalorro <aalorro@gmail.com>
2026-03-15 22:12:22 -0400

    Fix import statement for Hilt ViewModel in MainActivity

--- app/src/main/java/com/artmondo/algomodo/MainActivity.kt
+++ app/src/main/java/com/artmondo/algomodo/MainActivity.kt
@@ -14,7 +14,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.hilt.navigation.compose.hiltViewM
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.artmondo.algomodo.ui.screens.MainScreen
 import com.artmondo.algomodo.ui.screens.SplashScreen
